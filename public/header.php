9e2af2674de8134e47fc7082d2212f09602aca71
Randall <randms07@hotmail.com>
2021-06-14 18:31:49 -0600

    montando el header y footer

---
+++ public/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>principal</Header></title>
+    <link rel="stylesheet" type="text/css" href="public/css/bootstrap.css"/>
+        <script type="text/javascript" src="public/js/jquery.js"></script>
+</head>
+<body>
+<h1>Titulo</h1>
+    
+</body>
+</html>
---
+++ view/indexView.php
@@ -0,0 +1,9 @@
+<?php
+    include_once './public/header.php';
+?>
+
+
+
+<?php
+    include_once './public/footer.php';
+?>
